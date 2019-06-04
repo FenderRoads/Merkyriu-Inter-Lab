1e86ea014ecec58752d9c0d006d8bc650158d06c
FenderRoads <48343809+FenderRoads@users.noreply.github.com>
2019-06-04 15:16:36 +0400

    Updates

---
+++ js/common.js
@@ -0,0 +1,19 @@
+$(function() {
+  // Slider
+
+  $(".carousel1").owlCarousel({
+    loop: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    smartSpeed: 500,
+    responsive: {
+      0: {
+        items: 1
+      }
+    }
+  });
+
+  // Slider_
+});
