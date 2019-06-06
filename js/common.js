9d65e3b5dbd4dcd3c9ecb0257a0400ea403ba2f2
FenderRoads <48343809+FenderRoads@users.noreply.github.com>
2019-06-06 13:48:54 +0400

    Updates

--- js/common.js
+++ js/common.js
@@ -17,4 +17,59 @@ $(function() {
   });
 
   // Slider_
+
+  // Mobile menu
+
+  $(".dn-menu__hamb").on("click", function() {
+    $(".drop-down-menu").toggle(500);
+  });
+
+  $(".dn-menu__search").on("click", function() {
+    $("#search")
+      .css({
+        "-webkit-transition": "all .2s linear",
+        "width": "212px"
+      })
+      .focusout(function() {
+        $("#search").css({
+          "-webkit-transition": "all .2s linear",
+          "width": "44px"
+        });
+        $("#searchfas").css({
+          "-webkit-transition": "all .2s linear",
+          "left": "14px"
+        });
+        $('.dn-menu__vertline').css({
+          "-webkit-transition": "all .2s linear",
+          "opacity": "1"
+        })
+      });
+    $("#searchfas")
+      .css({
+        "-webkit-transition": "all .2s linear",
+        "left": "183px"
+      })
+    $('.dn-menu__vertline').css({
+      "-webkit-transition": "all .2s linear",
+      "opacity": "0"
+    })
+  });
+
+  // Mobile menu_
+
+  // Button_up
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
+      $('.top').addClass('active');
+    } else {
+      $('.top').removeClass('active');
+    }
+  });
+  $('.top').on('click', function() {
+    $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+  })	
+
+  // Button_up_
+
+
 });
