da8c66cc759b3405971260da0b26d01c0425aa38
FenderRoads <48343809+FenderRoads@users.noreply.github.com>
2019-06-11 12:18:49 +0400

    Update

--- app/js/common.js
+++ app/js/common.js
@@ -60,34 +60,60 @@ $(function() {
   // Button_up
   $(window).scroll(function() {
     if ($(this).scrollTop() > 300) {
-      $('.top').addClass('active');
+      $(".top").addClass("active");
     } else {
-      $('.top').removeClass('active');
+      $(".top").removeClass("active");
     }
   });
-  $('.top').on('click', function() {
-    $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
-  })	
+  $(".top").on("click", function() {
+    $("html, body")
+      .stop()
+      .animate({ scrollTop: 0 }, "slow", "swing");
+  });
 
   // Button_up_
 
   // Menu-fixed
-  
-  
-  $(window).scroll(function () {
-		if ($(this).scrollTop() > 207.05) {
-      $('.top-line').addClass('topline-fixed');
-      $('.top-line').css({'box-shadow': 'none'});
-      $('.top-line-bg').fadeIn(1);
-      $('.menu-wrapper').addClass('menu-wrapper-fixed')
-      $('.menu-wrapper-bg').fadeIn(1);
-		} else {
-      $('.top-line').removeClass('topline-fixed');
-      $('.top-line-bg').fadeOut(1);
-      $('.menu-wrapper').removeClass('menu-wrapper-fixed')
-      $('.menu-wrapper-bg').fadeOut(1);
-		}
-	});
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 207.05) {
+      $(".top-line").addClass("topline-fixed");
+      $(".top-line").css({ "box-shadow": "none" });
+      $(".top-line-bg").fadeIn(1);
+      $(".menu-wrapper").addClass("menu-wrapper-fixed");
+      $(".menu-wrapper-bg").fadeIn(1);
+    } else {
+      $(".top-line").removeClass("topline-fixed");
+      $(".top-line-bg").fadeOut(1);
+      $(".menu-wrapper").removeClass("menu-wrapper-fixed");
+      $(".menu-wrapper-bg").fadeOut(1);
+    }
+  });
   // Menu-fixed
 
+  // Accordeon
+
+  // $('.main-list > li').hide().prev().on('click', function() {
+  //   $('.elem-7-level-2').not(this).slideUp(500)
+  //   $(this).next().not(":visible").slideDown()
+  // })
+
+  // Accordeon_
+
+  // Range
+
+  $("input[name=multirange]").nativeMultiple({
+    stylesheet: "slider",
+    onCreate: function() {
+      console.log(this);
+    },
+    onChange: function(first_value, second_value) {
+      console.log("onchange", [first_value, second_value]);
+    },
+    onSlide: function(first_value, second_value) {
+      console.log("onslide", [first_value, second_value]);
+    }
+  });
+
+  // Range_
 });
--- gulpfile.js
+++ gulpfile.js
@@ -46,6 +46,7 @@ gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/jquery.min.js',
 		'app/libs/owlcarousel/owl.carousel.min.js',
+		'app/libs/nativeMultiple/nativemultiple.jquery.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
